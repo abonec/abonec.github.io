d9c8cfb07e6729f60643b31a440d410441169ade
Alexander Baronec <abonec@gmail.com>
2017-11-12 22:03:17 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,135 @@
+const image_url = "https://topmission-storage.s3.amazonaws.com/image/file/21527386/normal_photo.jpg";
+const image_width = 1200;
+const image_height = 900;
+
+
+const div = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+const lineFunction = d3.line()
+  .x(function (d) {
+    return d.x;
+  })
+  .y(function (d) {
+    return d.y;
+  });
+
+
+function createSvg(img) {
+  return d3.select("body").append("svg")
+    .attr("width", image_width)
+    .attr("height", image_height);
+  // .style("background-image", `url("${img}")`)
+  // .style("background-size", "cover")
+  // .style("background-repeat", "no-repeat")
+  // .style("background-position", "center center");
+}
+
+function addImage(container, url) {
+  return container.append('svg:image')
+    .attr('xlink:href', url)
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', image_width)
+    .attr('height', image_height);
+}
+
+function addBox(container, coordinates) {
+  const {x, y, width, height, isValid} = coordinates;
+  if (isValid) {
+    return container.append("rect")
+      .attr("x", x)
+      .attr("y", y)
+      .attr("width", width)
+      .attr("height", height)
+      .style("stroke-width", '1px')
+      .attr("opacity", '0.6')
+      ;
+  } else {
+    console.log(`${JSON.stringify(coordinates)} is invalid`);
+    return container;
+  }
+}
+
+const defaultStrokeWidth = 2;
+const selectedStrokeWidth = 4;
+
+function addBoundingBox(container, sku, boxCoordinates) {
+  const coordinates = convertCoordinates(boxCoordinates);
+  const path = convertCoordinatesToPath(coordinates);
+  const bbox = container.append('path').attr('d', lineFunction(path)).attr('stroke', 'blue').attr('stroke-width', defaultStrokeWidth).attr('fill', 'none');
+  const box = addBox(container, coordinates);
+
+  box.on('mouseover', () => {
+    bbox.attr('stroke-width', selectedStrokeWidth);
+    div.transition()
+      .duration(200)
+      .style("opacity", .9);
+    div.html(sku.name)
+      .style("left", (d3.event.pageX) + "px")
+      .style("top", (d3.event.pageY - 28) + "px");
+  }).on('mouseout', () => {
+    bbox.attr('stroke-width', defaultStrokeWidth);
+    div.transition()
+      .duration(500)
+      .style("opacity", 0);
+  })
+
+}
+
+window.max_x = 0;
+window.max_y = 0;
+
+const diff_x = -60;
+const diff_y = 40;
+
+function convertCoordinates(coordinates) {
+  let {x1, y1, x2, y2} = coordinates;
+  x1 = +x1 + diff_x;
+  x2 = +x2 + diff_x;
+  y1 = +y1 + diff_y;
+  y2 = +y2 + diff_y;
+
+  if (x1 > x2 || y2 < y1) {
+    return {isValid: false};
+  }
+  const x = x1;
+  const y = y1;
+  const width = x2 - x1;
+  const height = y2 - y1;
+  if (height < 0) {
+    debugger;
+  }
+  return {x, y, width, height, isValid: true};
+}
+
+function convertCoordinatesToPath(coordinates) {
+  const {x, y, width, height} = coordinates;
+  return [
+    {x, y},
+    {x: x + width, y},
+    {x: x + width, y: y + height},
+    {x, y: y + height},
+    {x, y},
+    {x: x + width, y},
+  ];
+
+}
+
+const container = createSvg();
+addImage(container, image_url);
+
+const photos = test_json.result.photos;
+for (const image_tag in photos) {
+  if (photos.hasOwnProperty(image_tag)) {
+    const photo = photos[image_tag];
+    photo.forEach((sku) => {
+      const boxes = sku.box;
+      boxes.forEach((box) => {
+        addBoundingBox(container, sku, box);
+      });
+    });
+  }
+}
+
---
+++ js/test_json.js
@@ -0,0 +1,331 @@
+window.test_json =
+  {
+    "result": {
+      "photo_state": {"5a0446f2456e2-8393": "successfully"},
+      "photos": {
+        "5a0446f2456e2-8393": [{
+          "id": "d7e0c7f3-3339-4c37-b7e7-42f023f26115",
+          "external_id": "cam_81827363",
+          "name": "Домик в деревне Молоко дер.отб. 3,5%-4,5%, Бутылка, .930",
+          "category_id": "36d72f19-f43e-4394-8835-3c194d01e584",
+          "category_name": "Традиционная молочная продукция",
+          "facing": "3",
+          "facing_group": "3",
+          "group_id": "7",
+          "plan": "0",
+          "price": "0.00",
+          "price_type": "0",
+          "width.cm": "159",
+          "box": [{"x1": "676", "x2": "734", "y1": "532", "y2": "678"}, {
+            "x1": "570",
+            "x2": "628",
+            "y1": "532",
+            "y2": "674"
+          }, {"x1": "623", "x2": "681", "y1": "530", "y2": "677"}]
+        }, {
+          "id": "ed9e84bb-d398-4379-9842-c753262ef928",
+          "external_id": "21170943",
+          "name": "Домик в деревне Молоко 0,5%, Тетра, .950",
+          "category_id": "d444e24a-dbe4-40a9-b878-32ac22337c05",
+          "category_name": "Теплая полка",
+          "facing": "3",
+          "facing_group": "3",
+          "group_id": "1",
+          "plan": "0",
+          "price": "0.00",
+          "price_type": "0",
+          "width.cm": "177",
+          "box": [{"x1": "171", "x2": "225", "y1": "181", "y2": "288"}, {
+            "x1": "325",
+            "x2": "372",
+            "y1": "177",
+            "y2": "288"
+          }, {"x1": "370", "x2": "425", "y1": "173", "y2": "290"}]
+        }, {
+          "id": "83007877-2c0f-4f6a-9298-33c3abe4106d",
+          "external_id": "59939427",
+          "name": "Народный Молоко паст.3,2% , Бутылка, .900",
+          "category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "category_name": "молочная продукция",
+          "facing": "4",
+          "facing_group": "4",
+          "group_id": "11",
+          "plan": "0",
+          "price": "69.00",
+          "price_type": "0",
+          "width.cm": "212",
+          "box": [{"x1": "888", "x2": "944", "y1": "324", "y2": "473"}, {
+            "x1": "937",
+            "x2": "995",
+            "y1": "322",
+            "y2": "473"
+          }, {"x1": "778", "x2": "833", "y1": "528", "y2": "688"}, {"x1": "728", "x2": "783", "y1": "528", "y2": "685"}]
+        }, {
+          "id": "3c11eb1d-0c19-44de-bd83-46d4b95ae9ef",
+          "external_id": "14726428",
+          "name": "Вкуснотеево Молоко ул/паст.3,2%, Бутылка, .900",
+          "category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "category_name": "молочная продукция",
+          "facing": "3",
+          "facing_group": "3",
+          "group_id": "12",
+          "plan": "0",
+          "price": "78.00",
+          "price_type": "0",
+          "width.cm": "123",
+          "box": [{"x1": "969", "x2": "1032", "y1": "525", "y2": "689"}, {
+            "x1": "1075",
+            "x2": "1144",
+            "y1": "538",
+            "y2": "692"
+          }, {"x1": "1023", "x2": "1083", "y1": "532", "y2": "691"}]
+        }, {
+          "id": "58c74217-ff4c-4d7b-b028-38fed3f184d3",
+          "external_id": "a0d0Y000000ANZ9QAO",
+          "name": "Prostokvashino Молоко паст 2,5%, ПЭТ, .930",
+          "category_id": "df44a7b9-58ff-4a95-a2fc-9a6d1015ce35",
+          "category_name": "Традиционный молочный продукт",
+          "facing": "1",
+          "facing_group": "1",
+          "group_id": "10",
+          "plan": "1",
+          "price": "0.00",
+          "price_type": "0",
+          "width.cm": "47",
+          "box": [{"x1": "839", "x2": "896", "y1": "328", "y2": "467"}]
+        }, {
+          "id": "4894915a-8198-4bbd-b63b-f549e33d43d3",
+          "external_id": "32538261",
+          "name": "Домик в деревне Молоко стер.2,5%, Тетра, .950",
+          "category_id": "d444e24a-dbe4-40a9-b878-32ac22337c05",
+          "category_name": "Теплая полка",
+          "facing": "2",
+          "facing_group": "2",
+          "group_id": "6",
+          "plan": "0",
+          "price": "0.00",
+          "price_type": "0",
+          "width.cm": "118",
+          "box": [{"x1": "425", "x2": "475", "y1": "177", "y2": "283"}, {
+            "x1": "474",
+            "x2": "526",
+            "y1": "172",
+            "y2": "278"
+          }]
+        }, {
+          "id": "29f01fac-a3f3-485a-bf4b-ef794d22bd0e",
+          "external_id": "37342977",
+          "name": "Село Зеленое Молоко 3,2%, Бутылка, 2000",
+          "category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "category_name": "молочная продукция",
+          "facing": "10",
+          "facing_group": "10",
+          "group_id": "3",
+          "plan": "0",
+          "price": "57.00",
+          "price_type": "0",
+          "width.cm": "650",
+          "box": [{"x1": "254", "x2": "306", "y1": "321", "y2": "463"}, {
+            "x1": "180",
+            "x2": "234",
+            "y1": "514",
+            "y2": "674"
+          }, {"x1": "902", "x2": "969", "y1": "508", "y2": "689"}, {
+            "x1": "282",
+            "x2": "344",
+            "y1": "521",
+            "y2": "674"
+          }, {"x1": "233", "x2": "287", "y1": "516", "y2": "674"}, {
+            "x1": "303",
+            "x2": "361",
+            "y1": "320",
+            "y2": "465"
+          }, {"x1": "128", "x2": "187", "y1": "519", "y2": "676"}, {
+            "x1": "203",
+            "x2": "257",
+            "y1": "318",
+            "y2": "462"
+          }, {"x1": "830", "x2": "905", "y1": "508", "y2": "692"}, {"x1": "158", "x2": "200", "y1": "326", "y2": "460"}]
+        }, {
+          "id": "d94b5925-8869-431e-a01a-e36176b815eb",
+          "external_id": "15550677",
+          "name": "Домик в деревне Молоко паст.2,5%, Бутылка, .930",
+          "category_id": "36d72f19-f43e-4394-8835-3c194d01e584",
+          "category_name": "Традиционная молочная продукция",
+          "facing": "5",
+          "facing_group": "5",
+          "group_id": "5",
+          "plan": "0",
+          "price": "0.00",
+          "price_type": "0",
+          "width.cm": "265",
+          "box": [{"x1": "583", "x2": "637", "y1": "333", "y2": "464"}, {
+            "x1": "739",
+            "x2": "792",
+            "y1": "332",
+            "y2": "467"
+          }, {"x1": "789", "x2": "842", "y1": "336", "y2": "468"}, {
+            "x1": "635",
+            "x2": "688",
+            "y1": "333",
+            "y2": "464"
+          }, {"x1": "688", "x2": "741", "y1": "336", "y2": "467"}]
+        }, {
+          "id": "6016ad53-6fb1-4f87-be4e-58d40af0e99a",
+          "external_id": "62279008",
+          "name": "Домик в деревне Молоко 6%, Тетра, .950",
+          "category_id": "d444e24a-dbe4-40a9-b878-32ac22337c05",
+          "category_name": "Теплая полка",
+          "facing": "2",
+          "facing_group": "2",
+          "group_id": "8",
+          "plan": "0",
+          "price": "0.00",
+          "price_type": "0",
+          "width.cm": "118",
+          "box": [{"x1": "528", "x2": "580", "y1": "179", "y2": "289"}, {
+            "x1": "577",
+            "x2": "630",
+            "y1": "179",
+            "y2": "290"
+          }]
+        }, {
+          "id": "0f82e192-569c-4df7-a64d-25a34dc337a1",
+          "external_id": "a0d0Y000000ANZdQAO",
+          "name": "Prostokvashino Молоко уп 3,2%, Тетра Square, .950",
+          "category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "category_name": "молочная продукция",
+          "facing": "2",
+          "facing_group": "2",
+          "group_id": "2",
+          "plan": "1",
+          "price": "0.00",
+          "price_type": "0",
+          "width.cm": "82",
+          "box": [{"x1": "212", "x2": "269", "y1": "176", "y2": "292"}, {
+            "x1": "280",
+            "x2": "326",
+            "y1": "178",
+            "y2": "289"
+          }]
+        }]
+      },
+      "share_shelf": {
+        "total": {"value": 0, "value_previous": 0},
+        "by_brands": [{
+          "is_own": "0",
+          "brand_id": "1126be28-5d11-4f8a-a189-bfe97e5044ef",
+          "brand_name": "Село Зеленое",
+          "brand_owner_id": "d6b3e344-aa0f-4a1a-a688-8e2fbce1b110",
+          "product_category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "value": 52,
+          "value_previous": 52,
+          "facing": "111/212"
+        }, {
+          "is_own": "0",
+          "brand_id": "2975c945-450d-4f0c-b72f-1cb88e8e54a7",
+          "brand_name": "Вкуснотеево",
+          "brand_owner_id": "15de7966-0331-4a73-895c-3a168ee6c350",
+          "product_category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "value": 10,
+          "value_previous": 10,
+          "facing": "21/212"
+        }, {
+          "is_own": "0",
+          "brand_id": "756d37ab-4daf-403c-b5ac-6e0609be4a3d",
+          "brand_name": "Other",
+          "brand_owner_id": "fda51285-1928-4c0c-9490-a19d2d3ea5ba",
+          "product_category_id": "df44a7b9-58ff-4a95-a2fc-9a6d1015ce35",
+          "value": 47,
+          "value_previous": 47,
+          "facing": "7/15"
+        }, {
+          "is_own": "0",
+          "brand_id": "a45f430d-9e3b-11e7-a5c2-000d3a250e47",
+          "brand_name": "Другое",
+          "brand_owner_id": "fda51285-1928-4c0c-9490-a19d2d3ea5ba",
+          "product_category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "value": 14,
+          "value_previous": 14,
+          "facing": "30/212"
+        }, {
+          "is_own": "0",
+          "brand_id": "afc795a4-f408-415b-95b0-4afde86c239b",
+          "brand_name": "Народный",
+          "brand_owner_id": "58be660d-7397-494c-943e-316618f2be69",
+          "product_category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "value": 17,
+          "value_previous": 17,
+          "facing": "36/212"
+        }, {
+          "is_own": "0",
+          "brand_id": "b7967cd8-77ea-41c5-bcc4-b69e8a65c223",
+          "brand_name": "Prostokvashino",
+          "brand_owner_id": "b734774e-652a-4c5a-8992-d6ca0eaf10bf",
+          "product_category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "value": 7,
+          "value_previous": 7,
+          "facing": "14/212"
+        }, {
+          "is_own": "0",
+          "brand_id": "b7967cd8-77ea-41c5-bcc4-b69e8a65c223",
+          "brand_name": "Prostokvashino",
+          "brand_owner_id": "b734774e-652a-4c5a-8992-d6ca0eaf10bf",
+          "product_category_id": "df44a7b9-58ff-4a95-a2fc-9a6d1015ce35",
+          "value": 53,
+          "value_previous": 53,
+          "facing": "8/15"
+        }, {
+          "is_own": "0",
+          "brand_id": "c0e69801-895a-4911-b078-c94dee5d9ee4",
+          "brand_name": "Домик в деревне",
+          "brand_owner_id": "5e436dad-e183-4e7a-a36e-57749997b056",
+          "product_category_id": "36d72f19-f43e-4394-8835-3c194d01e584",
+          "value": 100,
+          "value_previous": 100,
+          "facing": "72/72"
+        }, {
+          "is_own": "0",
+          "brand_id": "c0e69801-895a-4911-b078-c94dee5d9ee4",
+          "brand_name": "Домик в деревне",
+          "brand_owner_id": "5e436dad-e183-4e7a-a36e-57749997b056",
+          "product_category_id": "d444e24a-dbe4-40a9-b878-32ac22337c05",
+          "value": 100,
+          "value_previous": 100,
+          "facing": "70/70"
+        }],
+        "by_categories": [{
+          "product_category_id": "36d72f19-f43e-4394-8835-3c194d01e584",
+          "product_category_name": "Традиционная молочная продукция",
+          "value": 0,
+          "value_previous": 0,
+          "facing_own": "0/0",
+          "facing": "0/72"
+        }, {
+          "product_category_id": "8f0f1b83-8368-40f2-a908-1adeddc7923d",
+          "product_category_name": "молочная продукция",
+          "value": 0,
+          "value_previous": 0,
+          "facing_own": "0/0",
+          "facing": "0/212"
+        }, {
+          "product_category_id": "d444e24a-dbe4-40a9-b878-32ac22337c05",
+          "product_category_name": "Теплая полка",
+          "value": 0,
+          "value_previous": 0,
+          "facing_own": "0/0",
+          "facing": "0/70"
+        }, {
+          "product_category_id": "df44a7b9-58ff-4a95-a2fc-9a6d1015ce35",
+          "product_category_name": "Традиционный молочный продукт",
+          "value": 0,
+          "value_previous": 0,
+          "facing_own": "0/0",
+          "facing": "0/15"
+        }]
+      },
+      "method": "getAssortmentPackage",
+      "status": "end",
+      "package_id": "59fda3416adbc-2966"
+    }, "id": ""
+  };
